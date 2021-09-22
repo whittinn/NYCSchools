f23bae7b0b746e8b2a9db22c4514d015f83e243e
Nathaniel Whittington <whittinn@gmail.com>
2021-09-22 09:55:25 -0500

    Initial Commit

---
+++ NycSchools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NycSchools
+//
+//  Created by Nathaniel Whittington on 9/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
